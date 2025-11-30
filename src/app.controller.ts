44f06545d82386a2e42ed2932b3183d517e6fa2e
thanaphon0737 <thanapon081366@gmail.com>
2025-11-30 12:42:21 +0700

    feat: add controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('customers')
+  getCustomers() {
+    return this.appService.getCustomers();
+  }
+
+  @Post('customers/:id/reveal')
+  revealNationalId(@Param('id') id: number) {
+    return this.appService.getNationalId(Number(id));
   }
 }
