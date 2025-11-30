6172056fab3fb6877ce2369e7a18c79016161d8c
thanaphon0737 <thanapon081366@gmail.com>
2025-11-30 12:20:43 +0700

    feat: app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'password123',
+      database: 'secure_vault',
+      entities: [], // เราเน้น Raw SQL เลยไม่ใส่ Entity
+      synchronize: false, // ไม่ให้มันไปแก้ Table ของเรา
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
